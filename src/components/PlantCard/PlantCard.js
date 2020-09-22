f21a810510bcf106e601620f7c38d2f594371d3b
Aaron A. Abreu <a.modernfossil@gmail.com>
2020-09-22 14:54:46 -0400

    update card

--- src/components/PlantCard/PlantCard.js
+++ src/components/PlantCard/PlantCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './PlantCard.css';
 
 
-const PlantCard = ({ common_name, image_url, year, scientific_name, author, bibliography, onHideCard }) => {
+const PlantCard = ({ family, common_name, image_url, year, scientific_name, author, bibliography, onHideCard }) => {
   return (
     <div className='center pt4' >
       <div>
@@ -11,8 +11,9 @@ const PlantCard = ({ common_name, image_url, year, scientific_name, author, bibl
             <img src={image_url} className="w-100 image" alt={common_name} />
             <h1>{common_name}</h1>
             <cite className="scientific_name">{scientific_name} - {year}</cite>
-            <p className="family">{author}</p>
-            <p className="genus"> {bibliography} </p>
+            <p className="family">{family}</p>
+            <p className="bib">{author}-{bibliography} </p>
+            <cite className="citation">all data provided by: trefle.io</cite>
             <button
               onClick={onHideCard}>close</button>
           </div>
