d5206873c19b6edce809a1db7847c708c8a148db
Aaron A. Abreu <a.modernfossil@gmail.com>
2020-09-22 15:24:50 -0400

    form update

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -9,14 +9,14 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
       <p className='f3'>
         {'try searching for plants!'}
       </p>
-      <form id="search" action="#" className='center'>
+      <div id="search" className='center'>
         <div className='pa4 br3 shadow-5 center form'>
           <input className='f4 pa2 w-70' type="tex" onChange={onInputChange} />
           <button
             className='w-30 grow f4 link ph3 pv2 white bg-black'
             onClick={onButtonSubmit}>search</button>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
